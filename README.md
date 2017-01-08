c4a33ce343c825f18fb41132d5dbbc2f72e879a4
mail <you@example.com>
2017-01-08 21:14:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Xzurmo (ZURMO)
+
+SUPPLY: 8000000
+Block Size: 2 MB 
+FULL POS
+3 Hours minimum stake age
+Unlimited Max stake age
+Port:4466
+Rpcport:4467
---
+++ src/genesis.h
@@ -0,0 +1,13 @@
+#ifndef GENESIS_H
+#define GENESIS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+
+static const uint256 hashGenesisBlock("0x000002cbd50fdd3335b09a3e1d4bf69005d8aca84cb1e0490030b077e523e9f9");
+static const uint256 hashGenesisBlockTestNet("");
+
+
+
+#endif // GENESIS_H
